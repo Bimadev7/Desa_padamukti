ff655221bce288ef1ffb1dafc41601391f4ee314
Hilyaturrizqiyah <hilyaturrizqiyah00@gmail.com>
2024-06-19 13:31:46 +0700

    [IMPROVE] updating view profil desa

--- resources/views/public/sejarah.blade.php
+++ resources/views/public/sejarah.blade.php
@@ -19,7 +19,7 @@
 
 <div class="container">
     <article class="blog-post">
-        <h2 class="display-5 link-body-emphasis mb-4 text-center">Sample blog post</h2>
+        <h2 class="display-5 link-body-emphasis mb-4 text-center">Sejarah Desa</h2>
         <p>This blog post shows a few different types of content that’s supported and styled with Bootstrap. Basic typography, lists, tables, images, code, and more are all supported as expected.</p>
         <hr>
         <p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
