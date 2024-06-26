273aeefa501161336f5d6d96a6a805fd9cac8290
azmimuz99 <159402420+azmimuz99@users.noreply.github.com>
2024-06-26 22:33:52 +0700

    geografis

--- resources/views/public/geografis.blade.php
+++ resources/views/public/geografis.blade.php
@@ -22,21 +22,14 @@
                 <h2 class="display-5 link-body-emphasis mb-7 text-center">Informasi Geografis Desa Padamukti</h2>
                 <p><strong>Lokasi Geografis:</strong></p>
                 <ul>
-                    <li><strong>Koordinat:</strong>7°00'31.6"S 107°46'17.2"E</li>
-                    <li><strong>Luas Wilayah:</strong> 263,4 ha/m²</li>
-                    <li><strong>Topografi:</strong> Dataran rendah</li>
-                    <li><strong>Ketinggian:</strong> 670 mdpl</li>
+                    <li><strong>Koordinat:</strong>-7.24472830646825, 107.8169834044204</li>
+                    <li><strong>Topografi:</strong> Dataran Tinggi</li>
+                    <li><strong>Ketinggian:</strong> 967 mdpl</li>
                     <li><strong>Administrasi:</strong></li>
                     <ul>
-                        <li><strong>Kecamatan:</strong> Solokan Jeruk</li>
-                        <li><strong>Kabupaten:</strong> Bandung</li>
+                        <li><strong>Kecamatan:</strong> Pasirwangi</li>
+                        <li><strong>Kabupaten:</strong> Garut</li>
                         <li><strong>Provinsi:</strong> Jawa Barat</li>
-                    </ul>
-                    <li><strong>Potensi dan Industri:</strong></li>
-                    <ul>
-                        <li><strong>Pertanian:</strong> Sawah, kebun sayuran (mentimun, tomat, ubi jalar, singkong)</li>
-                        <li><strong>Industri:</strong> Home Industri (pengrajin kerudung, pengrajin ciput)</li>
-                        <li><strong>Pariwisata:</strong> Villa Kancil</li>
                     </ul>
                 </ul>
             </article>
@@ -45,8 +38,8 @@
             <article class="blog-post">
                 <h2 class="display-5 link-body-emphasis mb-4 text-center">Peta Lokasi Desa Padamukti</h2>
                 <div class="info w-100">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9599.715435800841!2d107.75721164857325!3d-7.012227205968178!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68c6aee51c0af3%3A0xae7bc5e161ed42f9!2sPadamukti%2C%20Solokanjreruk%2C%20Bandung%20Regency%2C%20West%20Java!5e0!3m2!1sen!2sid!4v1718207269561!5m2!1sen!2sid" frameborder="0" style="border:0; width: 100%; height: 400px;" allowfullscreen></iframe>
-                </div>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15832.109101662118!2d107.80800175113826!3d-7.237728148650926!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68bad158ba1a2d%3A0xe5259a25a206e508!2sPadamukti%2C%20Sukaresmi%2C%20Garut%20Regency%2C%20West%20Java!5e0!3m2!1sen!2sid!4v1719412487022!5m2!1sen!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>            </div>
+              </div>
             </article>
         </div>
     </div>
