abb14f370f1ff8db4509fbda05300e284f885e97
bimaf2 <bimantorof27@gmail.com>
2024-06-28 15:12:48 +0700

    tes

--- app/Models/Demografidesa.php
+++ app/Models/Demografidesa.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class DemografiDesa extends Modeldemografi_desa
+class DemografiDesa extends Model
 {
     use HasFactory;
     protected $table = 'demografi_desa';
