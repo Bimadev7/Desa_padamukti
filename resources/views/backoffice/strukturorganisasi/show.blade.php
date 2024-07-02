7f913f2ba17df0a0f184f7cda42ab921b46897e0
bimaf2 <bimantorof27@gmail.com>
2024-07-02 10:29:11 +0700

    perbaika md

--- resources/views/backoffice/strukturorganisasi/show.blade.php
+++ resources/views/backoffice/strukturorganisasi/show.blade.php
@@ -4,7 +4,7 @@
 <!-- Main content -->
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-11">
             <div class="card mt-5">
                 <div class="card-header">
                     <h3 class="card-title">Detail berita</h3>
